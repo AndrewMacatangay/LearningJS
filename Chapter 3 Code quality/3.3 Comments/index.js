e05aa68b408ab7effdd954e03fabf504de7c66e2
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-10-22 18:15:30 -0500

    Finished 3.3

---
+++ Chapter 3 Code quality/3.3 Comments/index.js
@@ -0,0 +1,16 @@
+//Comments can be used to describe how and why some piece of code works
+
+//Bad comments: You shouldn't need to specify what the code does. Instead,
+//you should specify how it works
+
+function multiplyBy2One(x)
+{
+	//Mutiplies a number by two
+	return x * 2;
+}
+
+//This function uses the arithmeticc left shift to multiply a number by 2
+function multiplyBy2Two(x)
+{
+	return x << 1;
+}
