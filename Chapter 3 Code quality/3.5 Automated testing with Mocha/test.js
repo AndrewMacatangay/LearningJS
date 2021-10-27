8dda8d94e0caa8b2844e9694eb20888d62386d5e
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-10-27 15:22:34 -0500

    Finished 3.5

--- Chapter 3 Code quality/3.5 Automated testing with Mocha/test.js
+++ Chapter 3 Code quality/3.5 Automated testing with Mocha/test.js
@@ -20,4 +20,54 @@ describe("pow", function()
 	{
 		assert.equal(pow(3, 4), 81);
 	});
+
+	it("Multiple numbers, power of 2", function()
+	{
+		for(let x = 1; x < 6; x++)
+		{
+			let expected = 1;
+			for(let y = 0; y < x; y++)
+				expected <<= 1;
+			assert.equal(pow(2, x), expected);
+		}
+	});
+
+	//Nested "describes" can be used to create a subgroup of test cases
+	describe("nested", function()
+	{
+		it("inner test", function()
+		{
+			assert.equal(pow(2, 5), 32);
+		});
+	});
 });
+
+//Functions can be created and executed before or after each test case.
+//You can execute them before all, before each, after all, or after each
+//test case
+describe("Before/After", function()
+{
+	before(() => console.log("Print before all"));
+	after(() => console.log("Print after all"));
+	beforeEach(() => console.log("Print before each"));
+	afterEach(() => console.log("Print after each"));
+
+	it("First", function()
+	{
+		console.log("first");
+	});
+
+	it("Second", function()
+	{
+		console.log("second");
+	});
+});
+
+//Other assertions include:
+//assert.equal(value1, value2)
+//assert.strictEqual(value1, value2)
+//assert.notEqual(value1, value2)
+//assert.notStrictEqual(value1, value2)
+//assert.isTrue(value) //using ===
+//assert.isFalse(value) // using ===
+//More here https://www.chaijs.com/api/assert/
