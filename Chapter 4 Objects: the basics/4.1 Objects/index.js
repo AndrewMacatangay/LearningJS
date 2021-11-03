7eb05a36db83496b870cfa4ed1286400d86daf2f
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-11-03 14:43:38 -0500

    Finished 4.1

--- Chapter 4 Objects: the basics/4.1 Objects/index.js
+++ Chapter 4 Objects: the basics/4.1 Objects/index.js
@@ -33,6 +33,7 @@ let example3 = "name";
 console.log(Person[example3]);
 
 //Computed properties: Property names computed during runtime
+//You can also add strings within the square brackets
 let example4 = "Favorite food";
 let Person2 = 
 {
@@ -43,3 +44,62 @@ console.log(Person2["Favorite food"]);
 
 //Print out all properties of the object
 console.log(Person);
+
+//Property value shorthand: Typically properties and variables have the same name and may
+//be modeled like this:
+
+function makeObject(name, age)
+{
+	return {
+	               name,
+		       age: age
+	       };
+}
+
+let example5 = makeObject("Andrew", 23);
+console.log(example5["name"]);
+console.log(example5["age"]);
+
+//There aren't really restrictions on property names since they are modeled as strings
+//You can use keywords as property names and even numbers
+
+//We can also check if a property exists with an object
+
+if(example5.weight === undefined)
+	console.log("Does not exist!");
+else
+	console.log("Exists!");
+
+//You could also use the "in" keyword
+if("name" in example5)
+	console.log("Exists!");
+else
+	console.log("Does not exist!");
+
+//Sometimes a property may exist, but has an undefined value. In this case, checking strict
+//equality will not work properly. In this case, we use the "in" keyword
+
+//We can use "for in" loops to loop through the properties of the object
+for(let key in example5)
+{
+	console.log(key);
+	console.log(example5[key]);
+}
+
+//Properties are sorted in ascending order if the property can be modeled as a Number
+//Otherwise, properties are sorted in creation order
+//Numbers first, then any other property afterwards!
+//You may add a '+' before a number to have it not be interpretted as a number
+
+let numbers = 
+{
+	"4": 4,
+	"2": 2,
+	"name": "Andrew",
+	"5": 5,
+	"3": 3,
+	"age": 23
+};
+
+for(let key in numbers)
+	console.log(key);
