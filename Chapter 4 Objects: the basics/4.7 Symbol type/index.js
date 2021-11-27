70201d690cb7c9f9088cb0f83f146bf0e722a1f0
AndrewMacatangay <andrewmacatangay5@gmail.com>
2021-11-26 20:13:25 -0600

    Finished 4.7

--- Chapter 4 Objects: the basics/4.7 Symbol type/index.js
+++ Chapter 4 Objects: the basics/4.7 Symbol type/index.js
@@ -26,4 +26,43 @@ user[ID1] = 1;
 user[ID2] = 2;
 console.log(user);
 
+//If we want to use a Symbol in an object literal, we can use square brackets
+let ID = Symbol("ID");
+let user1 = 
+{
+	name: "Andrew",
+	[ID]: 123
+};
 
+console.log(user1);
+
+//Symbols are skipped in for-in loops or Object.keys(?). The reason for this is because
+//symbols are treated as hidden properties of an object.
+
+for (let key in user1)
+	console.log(key);
+console.log(Object.keys(user1));
+
+//If we use Object.assign(), it will copy both the string and symbol properties
+let clone = Object.assign({}, user1);
+console.log(clone);
+
+//We can use global symbols if we want to use the same-named symbols for different objects.
+//In the code below, we are comparing the local symbol with description "ID", with the global
+//symbol. They are not the same Symbol. The .for() function will create a new global symbol
+//if one does not exist. Otherwise, it will return the one that exists
+
+let globalSymbol = Symbol.for("ID");
+console.log(globalSymbol === ID1);
+
+let globalSymbol2 = Symbol.for("ID");
+console.log(globalSymbol === globalSymbol2);
+
+//We can use the .keyFor() function to find the key for a global function. If the key
+//does not exist, then undefined will be returned.
+
+console.log(Symbol.keyFor(globalSymbol));
+console.log(Symbol.keyFor(ID1));
+
+//There are many member functions for the Symbol class that might be useful. It is advised
+//to look over them for further detail.
